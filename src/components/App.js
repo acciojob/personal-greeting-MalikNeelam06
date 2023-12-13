d52e10448eb62b35fea792a8ace3229259dcb842
NeelamMalik <nmalik060191@gmail.com>
2023-12-13 17:13:32 +0530

    commit1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
 import React from "react";
+import { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let[value,setValue]=useState("");
+  let[greeting,setGreeting]=useState("");
+  function implement(e){
+setValue(e.target.value);
+setGreeting(`Hello ${value}!`);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <form>
+          <label>Enter your name:</label>
+          <input onChange={implement}></input>
+        </form>
+        <p>{greeting}</p>
     </div>
   )
 }
